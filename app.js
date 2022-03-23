a39e9d4e8f65fae33e465b6eba55bf7d6a313277
dhia <diaminiaoui@gmail.com>
2022-03-23 23:27:20 +0100

    23/03

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
-  
+
 app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images/accessories', express.static(path.join(__dirname, 'public/images/accessories')));
 app.use('/images/articles', express.static(path.join(__dirname, 'public/images/articles')));
 app.use('/join-us/cv', express.static(path.join(__dirname, 'public/join-us-cv')));
-console.log(port);
+
 
 app.use(clientRouter);
 app.use(adminRouter);
